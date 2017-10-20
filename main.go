3353bec26b68a4d2d1958c6db4762a128d9b4490
Nayef Ghattas <nayef.ghattas@gmail.com>
2017-10-20 19:56:32 -0400

    Create the test

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "test/someotherpackage"
+
+func main() {
+    fmt.Println(someotherpackage.Map)
+}
---
+++ someotherpackage/test.go
@@ -0,0 +1,12 @@
+package someotherpackage
+
+type Test struct {
+    X int
+    Y int
+}
+
+var Map = map[Test]struct{}{
+    {1,2} : struct{}{},
+    {1,2} : struct{}{},
+    {1,2} : struct{}{},
+}
